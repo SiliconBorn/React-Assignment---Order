803e7d0f60be489c1ab359bb0f769b0d49acde26
biswash <just11hiro@gmail.com>
2023-01-13 09:13:17 +0530

    added dish adding functionality with filtering

--- src/components/FormThree.jsx
+++ src/components/FormThree.jsx
@@ -19,32 +19,50 @@ const FormThree = () => {
   const [selectedDishes,setSelectedDishes]=useState([])
   console.log(orderState)
 
-  const selectHandler =(e)=>{
-    console.log(e.target.value)
-    const dishToAdd = JSON.parse(e.target.value)
-    let dishAlreadySelected =false
-    if( selectedDishes.length>0){
-      dishAlreadySelected = selectedDishes.length>0 && selectedDishes.find((dishObj)=>dishObj.dish.id===dishToAdd.id)
-    }
-     if(dishAlreadySelected) return
-     else setSelectedDishes([...selectedDishes,{dish:dishToAdd,servings:1}])
+  const addDishBtnHandler = ()=>{
+     setSelectedDishes([...selectedDishes,{dish:allAvailableDishes[0],servings:1}])
   }
 
-  const servingsChangeHandler =(e)=>{
+  const selectHandler =(e,index,dishToupdateId,dishToupdateName)=>{
+    console.log(e.target.value)
+    const dishToAdd = JSON.parse(e.target.value)
+    let dishAlreadySelected = false
+    if( selectedDishes.length>0){
+      dishAlreadySelected =  selectedDishes.find((dishObj)=>dishObj.dish.id===dishToAdd.id)
+    }
+     if(dishAlreadySelected){
+
+        alert('dish already selected. Please increase the servings ,if same dish is needed')
+          e.target.value = selectedDishes[index].dish.name
+        setSelectedDishes([...selectedDishes]) 
+
+     } else {
+      const updatedSelectedDishes = selectedDishes.map((selectedDish)=>{
+        if(selectedDish.dish.id===dishToupdateId && selectedDish.dish.name ===dishToupdateName){
+         let updatedCurrentDish = {...selectedDish,dish:dishToAdd}
+         return updatedCurrentDish
+        }
+        return selectedDish
+  })
+
+  setSelectedDishes([...updatedSelectedDishes])
+     }
+  }
+
+  const servingsChangeHandler =(e,dishToupdateId,dishToupdateName)=>{
     console.log(e.target.value)
     const selectedServings = Number(e.target.value)
-    if(selectedDishes.length===0){
-      setSelectedDishes([{dish:allAvailableDishes[0],servings:selectedServings}])
-    }else{
-      const updatedSelectedDishes = selectedDishes.reduceRight((updatedSelectedDishes,current,index)=>{
-        if(index===0){
-          let updatedDish = {...current,servings:selectedServings}
-          updatedSelectedDishes=[updatedDish,...updatedSelectedDishes]
-        }else updatedSelectedDishes=[current,...updatedSelectedDishes]
-        return updatedSelectedDishes
-      },[])
-      setSelectedDishes([...updatedSelectedDishes])
-    }
+   
+         const updatedSelectedDishes = selectedDishes.map((selectedDish)=>{
+               if(selectedDish.dish.id===dishToupdateId && selectedDish.dish.name ===dishToupdateName){
+                let updatedCurrentDish = {...selectedDish,servings:selectedServings}
+                return updatedCurrentDish
+               }
+               return selectedDish
+         })
+
+         setSelectedDishes([...updatedSelectedDishes])
+    
   }
 
 useEffect(()=>{
@@ -58,14 +76,19 @@ useEffect(()=>{
       </div>
       <div className='steps-form-container'>
       <div className='steps-form'>
-        <div className='dish-selection-container'>
+        <div className='selected-dishes-container'>
+          {
+            selectedDishes.length>0 && selectedDishes.map((selectedDish,index)=>{
+              return(
+                <>
+                <div className='dish-selection-container'>
           <label htmlFor='dishSelect'>  Please select a dish</label>
-          <Form.Select onChange={selectHandler} name='dishSelect' >
+          <Form.Select onChange={(e)=>selectHandler(e,index,selectedDish.dish.id,selectedDish.dish.name)} name='dishSelect'  value={selectedDish.dish.name}>
             {
               allAvailableDishes.map((dish,index)=>{
                 return(
                   <>
-                  <option value={JSON.stringify(dish)} key={`${index}-${dish.name}-${dish.id}`}>{dish.name}</option>
+                  <option value={JSON.stringify(dish)} key={`${index}-${dish.name}-${dish.id}`} selected={selectedDish.dish.name===dish.name && selectedDish.dish.id === dish.id}>{dish.name}</option>
                   </>
                 )
               })
@@ -75,9 +98,17 @@ useEffect(()=>{
         <div className='serving-selection-container'>
          
             <label htmlFor='totalServings'> Please  enter number of servings</label>
-            <input type='number' min={1} max={10} name='totalServings' defaultValue={1} onChange={servingsChangeHandler}/>
+            <input type='number' min={1} max={10} name='totalServings' defaultValue={selectedDish.servings} onChange={(e)=>servingsChangeHandler(e,selectedDish.dish.id,selectedDish.dish.name)}/>
       
         </div>
+                </>
+              )
+            })
+          }
+        </div>
+      </div>
+      <div className='add-dishes-btn-container'>
+        <button className='add-dish-btn' onClick={addDishBtnHandler}>Add dish</button>
       </div>
      </div>
      <div className='steps-btn-container'>
