13a6ee6ec9a336a1cbba7b24acfcd665ab3dba22
biswash <just11hiro@gmail.com>
2023-01-14 20:54:09 +0530

    completed step two cleanup

--- src/components/FormTwo.jsx
+++ src/components/FormTwo.jsx
@@ -1,66 +1,82 @@
-import React, { useContext, useState } from 'react'
-import { useEffect } from 'react';
-import { Form } from 'react-bootstrap'
-import { OrderFormContext } from './App'
-import Header from './Header'
-import NextBtn from './NextBtn'
-import PrevBtn from './PrevBtn'
+import React, { useContext, useState } from "react";
+import { useEffect } from "react";
+import { OrderFormContext } from "./App";
+import NextBtn from "./NextBtn";
+import PrevBtn from "./PrevBtn";
 
 const FormTwo = () => {
-  const {orderState,orderDispatch,dishesData} = useContext(OrderFormContext)
-  const [allAvailableRestaurants] = useState(()=>{
-     const restaurantsAvailable = dishesData.dishes.reduce((restaurantsAvailable,current)=>{
-      if(current.availableMeals.includes(orderState.mealType) && !(restaurantsAvailable.includes(current.restaurant)) ){
-         restaurantsAvailable = [...restaurantsAvailable,current.restaurant]
-      }
-      return restaurantsAvailable
-     },[])
-     return restaurantsAvailable
-  })
+  /*REQ. STATES*/
+  const { orderState, orderDispatch, dishesData } =
+    useContext(OrderFormContext);
+  const [allAvailableRestaurants] = useState(() => {
+    const restaurantsAvailable = dishesData.dishes.reduce(
+      (restaurantsAvailable, current) => {
+        if (
+          current.availableMeals.includes(orderState.mealType) &&
+          !restaurantsAvailable.includes(current.restaurant)
+        ) {
+          restaurantsAvailable = [...restaurantsAvailable, current.restaurant];
+        }
+        return restaurantsAvailable;
+      },
+      []
+    );
+    return restaurantsAvailable;
+  });
 
-  useEffect(()=>{
-     if(!orderState.selectedRestaurant){
+  /*REQ. SIDE-EFFECT*/
+  useEffect(() => {
+    if (!orderState.selectedRestaurant) {
       orderDispatch({
-        type:"UPDATE_SELECTED_RESTAURANT",
-        payload:allAvailableRestaurants[0]
-      })
-     }
-  },[])
-  const selectHandler=(e)=>{
-    console.log(e.target.value)
+        type: "UPDATE_SELECTED_RESTAURANT",
+        payload: allAvailableRestaurants[0],
+      });
+    }
+  }, []);
 
+  /*REQ. FUNCTIONS*/
+  //FUNCTION TO HANDLE CHANGES IN SELECT ELEMENT
+  const selectHandler = (e) => {
     orderDispatch({
-      type:"UPDATE_SELECTED_RESTAURANT",
-      payload:e.target.value
-    })
-  }
+      type: "UPDATE_SELECTED_RESTAURANT",
+      payload: e.target.value,
+    });
+  };
 
   return (
-    <section className='order-second-step'>
-      <div className='steps-form-container center'>
-      <div className='steps-form'>
-        <div className='meal-selection-container'>
-          <label htmlFor='mealSelect'>  Please select a meal</label>
-          <Form.Select onChange={selectHandler} name='mealSelect' defaultValue={orderState.selectedRestaurant?orderState.selectedRestaurant:allAvailableRestaurants[0]}>
-            {
-              allAvailableRestaurants.map((restaurant,index)=>{
-                return(
+    <section className="order-second-step">
+      <div className="steps-form-container center">
+        <div className="steps-form">
+          <div className="meal-selection-container">
+            <label htmlFor="mealSelect">Select Your Desired Restaurant</label>
+            <select
+              onChange={selectHandler}
+              name="mealSelect"
+              value={
+                orderState.selectedRestaurant
+                  ? orderState.selectedRestaurant
+                  : allAvailableRestaurants[0]
+              }
+            >
+              {allAvailableRestaurants.map((restaurant, index) => {
+                return (
                   <>
-                  <option value={restaurant} key={`${index}-${restaurant}`}>{restaurant}</option>
+                    <option value={restaurant} key={`${index}-${restaurant}`}>
+                      {restaurant}
+                    </option>
                   </>
-                )
-              })
-            }
-          </Form.Select>
+                );
+              })}
+            </select>
+          </div>
         </div>
       </div>
-     </div>
-     <div className=' double steps-btn-container'>
-      <PrevBtn dispatch={orderDispatch}/>
-      <NextBtn dispatch={orderDispatch}/>
-     </div>
+      <div className=" double steps-btn-container">
+        <PrevBtn dispatch={orderDispatch} />
+        <NextBtn dispatch={orderDispatch} access={() => true} />
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default FormTwo
+export default FormTwo;
